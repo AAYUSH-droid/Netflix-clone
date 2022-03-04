3e5e63680136f0165e2154543e9cfd051a51b303
AAYUSH SHARMA <aayushsharma@AAYUSHs-MacBook-Air.local>
2022-03-04 17:07:34 +0530

    netflix frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuVcOuGV7qRjoNBL4-USlfevANJv8UNSE",
+    authDomain: "netflix-clone-d688c.firebaseapp.com",
+    projectId: "netflix-clone-d688c",
+    storageBucket: "netflix-clone-d688c.appspot.com",
+    messagingSenderId: "245729009268",
+    appId: "1:245729009268:web:8f42957f3cf5f6d9a13b21"
+  };
